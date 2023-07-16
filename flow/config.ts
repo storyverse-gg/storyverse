f89aae8a902035988cef74a6d3783bd29122e808
Tushar Ojha <tusharojha2001@gmail.com>
2023-07-17 03:42:51 +0530

    created landing page

--- flow/config.ts
+++ flow/config.ts
@@ -1,6 +1,6 @@
 import { config } from "@onflow/fcl";
 
 config({
-  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
-  "discovery.authn.endpoint": "https://fcl-discovery.onflow.org/api/testnet/authn"
+  "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://rest-mainnet.onflow.org"
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import "../flow/config";
 import { useState, useEffect } from "react";
 import * as fcl from "@onflow/fcl";
+import '../styles/Home.module.css';
 
 export default function Home() {
 
@@ -9,15 +11,25 @@ export default function Home() {
   useEffect(() => { fcl.currentUser.subscribe(setUser) }, [])
 
   return (
-    <div>
+    <div className='mt-12 body h-full p-4 flex flex-1 flex-col justify-between items-center text-center'>
       <Head>
         <title>Storyverse | Explore the world unkown to others | AI NFT Based Game</title>
         <meta name="description" content="World's first Generative AI based NFT Game in which player emerses themselves into text-based storyline and take decisions to progress on-chain." />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <h1 className="text-4xl font-bold underline">
-        Flow App
-      </h1>
+      <div className='flex flex-col flex-1 justify-center items-center'>
+        <div className='flex flex-1 justify-center items-center flex-col'>
+          <h1 className='text-4xl font-bold text-white leading-relaxed text-shadow'>Unleash Your Imagination, Forge Your Storyline, Collect NFT Memories!</h1>
+          <h2 className='text-xl text-white font-bold text-shadow'>A text-based game for story readers, thinkers and creators.</h2>
+          <div className='flex-row mt-8'>
+            <div className='btn btn-accent mr-2'>Read Docs</div>
+            <div className='btn' onClick={fcl.logIn}>Start Game</div>
+          </div>
+        </div>
+      </div>
+      <div className='flex justify-center items-center flex-col mb-12'>
+        <Image src={"/type.gif"} height={250} width={250} alt="type" />
+      </div>
     </div>
   );
 }
